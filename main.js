4ac173098420c7c15fd39d7c75e94ef18cc7a173
S. Hall <shall.usnavy@gmail.com>
2018-05-14 21:23:16 -0400

    Initial commit

---
+++ main.js
@@ -0,0 +1,8 @@
+/*
+1. I can see whether Free Code Camp is currently streaming on Twitch.tv.
+2. I can click the status output and be sent directly to the Free Code Camp's Twitch.tv 
+    channel.
+3. If a Twitch user is currently streaming, I can see additional details about what
+    they are streaming.
+URL: https://www.freecodecamp.org/challenges/use-the-twitchtv-json-api
+*/
