dd077e71a29cc1eaaa2f5b311b77fe6c9fdd9124
S. Hall <shall.usnavy@gmail.com>
2018-05-16 14:10:15 -0400

    Add instruction documentation to JS file

--- main.js
+++ main.js
@@ -6,4 +6,13 @@
     they are streaming.
 URL: https://www.freecodecamp.org/challenges/use-the-twitchtv-json-api
 API: https://wind-bow.glitch.me/
+    USAGE:
+Replace the Twitch API base URL https://api.twitch.tv/kraken with 
+https://wind-bow.gomix.me/twitch-api. Use this endpoint according to the Twitch API
+documentation.
+
+NOTE:
+This server caches data to lower the request rate. To prevent abuses this server accepts
+GET requests only, and serves only routes /users/:user, /channels/:channel, and
+/streams/:stream. These are more than enough to complete the challenge. 
 */
